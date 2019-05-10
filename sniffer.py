b301d2902f8b3d2ff2228c4903cdfecf1cc8f43e
prokorpio <sanchez.christopherjeff@gmail.com>
2019-05-10 12:21:28 +0800

    tidied up the code

--- sniffer.py
+++ sniffer.py
@@ -3,39 +3,35 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("listOfIP", nargs='+', \
+    parser.add_argument("list_of_IP", nargs='+', \
                         help='space separated IPs to track')
 
     args = parser.parse_args()
 
-    for idx, ip in enumerate(args.listOfIP):
+    for idx, ip in enumerate(args.list_of_IP):
         if idx == 0:
-            bpfFilter = 'host ' + args.listOfIP[0]
+            bpf_filter = 'host ' + args.list_of_IP[0]
         else:
-            bpfFilter = bpfFilter + ' or host ' + ip
-    #print info to track
+            bpf_filter = bpf_filter + ' or host ' + ip
 
-    cap = pyshark.LiveCapture(interface='en0', bpf_filter=bpfFilter, \
+    print('Tracking: ')
+    for ip in args.list_of_IP:
+        print('\t ',ip)
+
+    cap = pyshark.LiveCapture(interface='en0', bpf_filter=bpf_filter, \
              only_summaries=True, monitor_mode=True)
     cap.sniff(packet_count=10) #cap.sniff() to sniff forever (in a thread)
 
     bytes_received = {}
     bytes_sent = {}
-    for ip in args.listOfIP:
-    	bytes_received[ip] = 0;	#defines dictionary where key=IP, dict[key] = bytes[IP] = #bytes
-    	bytes_sent[ip] = 0;
+    for ip in args.list_of_IP:
+        bytes_received[ip] = 0;	#defines dictionary where key=IP, dict[key] = bytes[IP] = #bytes
+        bytes_sent[ip] = 0;
 
     for pkt in cap:
-    	if pkt.destination in args.listOfIP:
+        if pkt.destination in args.list_of_IP:
             bytes_received[pkt.destination] += int(pkt.length)
             print('rcvd: {}'.format(bytes_received))
-    	elif pkt.source in args.listOfIP:
+        elif pkt.source in args.list_of_IP:
             bytes_sent[pkt.source] += int(pkt.length)
             print('sent: {}'.format(bytes_sent))
-
-    # cap = pyshark.LiveCapture(interface='en0', bpf_filter='host 10.11.16.58', \
-    #         only_summaries=True, monitor_mode=True)
-    # cap.sniff(packet_count=10) #cap.sniff() to sniff forever (in a thread)
-    #
-    # for pkt in cap:
-    #     print(pkt)
