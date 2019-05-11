4b4829b0c103cc4dfc94ba8dd6c0b94fba8accae
prokorpio <sanchez.christopherjeff@gmail.com>
2019-05-12 00:06:36 +0800

    defined packet data object, and adjusted for multi-user tracking

--- sniffer.py
+++ sniffer.py
@@ -1,5 +1,18 @@
 import pyshark
 import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+class userData:
+    """ contains info per tracked user"""
+    #constructor
+    def __init__(self, ip):
+        self.ip = ip
+        self.time_stamp = []
+        self.bytes_rcvd_per_sec = []
+        self.bytes_sent_per_sec = []
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -18,20 +31,50 @@ if __name__ == '__main__':
     for ip in args.list_of_IP:
         print('\t ',ip)
 
+    # setup packet sniffer
     cap = pyshark.LiveCapture(interface='en0', bpf_filter=bpf_filter, \
-             only_summaries=True, monitor_mode=True)
-    cap.sniff(packet_count=10) #cap.sniff() to sniff forever (in a thread)
+             only_summaries=True, monitor_mode=False)
+    cap.sniff(timeout=0) #cap.sniff() to sniff forever (in a thread)
 
-    bytes_received = {}
-    bytes_sent = {}
+    user_list = []    #list of packetData objects
     for ip in args.list_of_IP:
-        bytes_received[ip] = 0;	#defines dictionary where key=IP, dict[key] = bytes[IP] = #bytes
-        bytes_sent[ip] = 0;
+        user_list.append(userData(ip))
 
-    for pkt in cap:
-        if pkt.destination in args.list_of_IP:
-            bytes_received[pkt.destination] += int(pkt.length)
-            print('rcvd: {}'.format(bytes_received))
-        elif pkt.source in args.list_of_IP:
-            bytes_sent[pkt.source] += int(pkt.length)
-            print('sent: {}'.format(bytes_sent))
+
+    sniff_duration = 3  # 3-second sniffing
+    iter_count = 0      # 1 sniff duration is one iter count
+    max_iter_count = 60 # end tracking after this much iterations
+    for user in user_list:  #initialize
+        user.bytes_rcvd_per_sec.append(0)
+        user.bytes_sent_per_sec.append(0)
+        user.time_stamp.append(0)
+    start_time = time.monotonic()
+    for pkt in cap: #iterate through sniffed packet
+        for user in user_list:  #iterate through user user_list
+            if user.ip == pkt.destination:
+                user.bytes_rcvd_per_sec[iter_count] += int(pkt.length)
+                print('pkt rcvd: ',int(pkt.length))
+            elif user.ip == pkt.source:
+                user.bytes_sent_per_sec[iter_count] += int(pkt.length)
+                print('pkt sent: ',int(pkt.length))
+
+        delta_time = time.monotonic() - start_time
+        if delta_time >= sniff_duration:
+            for user in user_list:
+                user.bytes_rcvd_per_sec[iter_count] /= delta_time
+                user.bytes_sent_per_sec[iter_count] /= delta_time
+                user.time_stamp[iter_count] = delta_time
+            #update plot
+            iter_count += 1
+            print('iteration: ', iter_count)
+            for user in user_list:
+                print('IP: ',user.ip)
+                print('\trcvd/s: ',user.bytes_rcvd_per_sec)
+                print('\tsent/s: ',user.bytes_sent_per_sec)
+                print('\ttime: ',user.time_stamp)
+                user.bytes_rcvd_per_sec.append(0)
+                user.bytes_sent_per_sec.append(0)
+                user.time_stamp.append(0)
+            if iter_count == max_iter_count:
+                break   # will stop iterating through sniffed packets
+            start_time = time.monotonic()
