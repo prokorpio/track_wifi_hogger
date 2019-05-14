8c6b413f5f85b378a9dacd2ee8406f300bf7d46e
prokorpio <sanchez.christopherjeff@gmail.com>
2019-05-14 09:15:29 +0800

    fixed the compressing time axis

--- sniffer.py
+++ sniffer.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 from datetime import datetime
+import matplotlib.dates as mdates
 
 
 class userData:
@@ -47,21 +48,20 @@ if __name__ == '__main__':
         user_list.append(userData(ip))
 
     sniff_duration = 1  # sniffing time
-    max_iter_count = 5 # end tracking after this much iterations
-
-    for user in user_list:  #initialize/reset, +1 because 0 @ 0th place
-        user.bytes_rcvd_per_sec.extend([0]*(max_iter_count+1))
-        user.bytes_sent_per_sec.extend([0]*(max_iter_count+1))
-        user.time_stamp.extend([0]*(max_iter_count+1))
 
     #plotter initialization
     plt.ion()
     fig = plt.figure()
     graph = fig.add_subplot(111)
-    window_size = 60*sniff_duration
+    window_size = 300*sniff_duration
     y_default = 100     #when the y-values inside the window < y_default, the y-axis maximum = y_default (i.e. minimum y_axis range)
     graph.axis([0, window_size, 0, y_default])  #setting the initial plot dimensions
+    #graph.xaxis.set_minor_locator(mdates.SecondLocator())
 
+    for user in user_list:  #initialize/reset, +1 because 0 @ 0th place
+        user.bytes_rcvd_per_sec.extend([0]*window_size)
+        user.bytes_sent_per_sec.extend([0]*window_size)
+        user.time_stamp.extend([datetime.utcnow()]*window_size)
     start_time = time.monotonic()
     try:
         while True:
@@ -82,42 +82,47 @@ if __name__ == '__main__':
                         user.bytes_rcvd_per_sec[-1] += int(pkt.length)
                     elif user.ip == pkt.source:
                         user.bytes_sent_per_sec[-1] += int(pkt.length)
-
+            time_now = datetime.utcnow()
             for user in user_list: #get time average
                 user.bytes_rcvd_per_sec[-1] /= delta_time
                 user.bytes_sent_per_sec[-1] /= delta_time
-                user.time_stamp[-1] = datetime.now().strftime('%H:%M:%S')
+                user.time_stamp[-1] = time_now
 
             #UPDATE PLOT
-            # graph.clear()   #clear the plot every iteration to give way to the new curves
-            # for user in user_list:
-            #     graph.plot(user.time_stamp, user.bytes_sent_per_sec, label=user.ip + ' (sent)')
-            #     graph.plot(user.time_stamp, user.bytes_rcvd_per_sec, label=user.ip + ' (recv)')
-            #     graph.legend(loc=2) #location = upper left
-            #
-            #     # please add plot title here
-            #     plt.xlabel('Time (s)')
-            #     plt.ylabel('Bytes')
-            #     plt.grid()
-            #
-            #     y_max = max(user.bytes_sent_per_sec[-int(window_size/sniff_duration):] + \
-            #          user.bytes_rcvd_per_sec[-int(window_size/sniff_duration):] + [y_default])  #y_max = max(sent or received bytes or y_default | in the past window_size second)
-            #     plt.ylim(0, y_max)
-            #     if user.time_stamp[iter_count] > window_size:   #move the window if iter_count reaches end of window
-            #         plt.xlim(user.time_stamp[iter_count]-window_size, user.time_stamp[iter_count])
-            #     else:
-            #         plt.xlim(0, window_size)
-            #
-            #     fig.canvas.draw()
-            #     plt.pause(0.0001)	# needed to be able to see plot
+
+            graph.clear()   #clear the plot every iteration to give way to the new curves
+            for user in user_list:
+                graph.plot(mdates.date2num(user.time_stamp), user.bytes_sent_per_sec, label=user.ip + ' (sent)')
+                graph.plot(mdates.date2num(user.time_stamp), user.bytes_rcvd_per_sec, label=user.ip + ' (recv)')
+                graph.legend(loc=2) #location = upper left #better if there'd be a list on the side
+
+                plt.title('Fig. 1')# please add plot title here
+                plt.xlabel('Time (s)')
+                plt.ylabel('Bytes per second (bps)')
+                plt.grid()
+                plt.gcf().autofmt_xdate()
+
+                y_max = max(user.bytes_sent_per_sec + user.bytes_rcvd_per_sec + [y_default])  #y_max = max(sent or received bytes or y_default)
+                plt.ylim(0, y_max + int(0.1*y_max))
+                #plt.xlim(user.time_stamp[0], user.time_stamp[-1])
+                graph.xaxis.set_major_locator(mdates.MinuteLocator())
+                graph.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+                graph.xaxis.set_minor_locator(mdates.SecondLocator(interval=10))
+                # if user.time_stamp[0] != 0:   #move the window if list is full
+                #     plt.xlim(user.time_stamp[0], user.time_stamp[-1])
+                # else:
+                #     plt.xlim(0, window_size)
+
+                fig.canvas.draw()
+                plt.pause(0.001) # needed to be able to see plot
 
             cap.clear()
 
-            for user in user_list:
-    #             print('Loop Time: ',time.monotonic()-relative_start_time)
-    #             print('IP: ',user.ip)
-                print('\trcvd/s: ',user.bytes_rcvd_per_sec)
-                print('\tsent/s: ',user.bytes_sent_per_sec)
-                print('\ttime: ',user.time_stamp)
+    #         for user in user_list:
+    # #             print('Loop Time: ',time.monotonic()-relative_start_time)
+    # #             print('IP: ',user.ip)
+    #             print('\trcvd/s: ',user.bytes_rcvd_per_sec)
+    #             print('\tsent/s: ',user.bytes_sent_per_sec)
+    #             print('\ttime: ',user.time_stamp)
     except KeyboardInterrupt:
-        print('\nExiting...')
+        print('\nExiting...') #better yata if i-exit na lang yung window
