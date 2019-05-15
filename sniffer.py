7f602ecd455adac545756e124c7e559770b187bc
prokorpio <sanchez.christopherjeff@gmail.com>
2019-05-15 15:06:51 +0800

    updated list shift for all users

--- sniffer.py
+++ sniffer.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
 
     # setup packet sniffer
     cap = pyshark.LiveCapture(interface=args.interface, bpf_filter=bpf_filter, \
-             only_summaries=True, monitor_mode=False)
+             only_summaries=True, monitor_mode=True)
 
     user_list = []    #list of packetData objects
     for ip in args.list_of_IP:
@@ -55,6 +55,7 @@ if __name__ == '__main__':
     fig = plt.figure(figsize=(10,5))
     graph = fig.add_subplot(111)
     window_size = int(60*5/sniff_duration) # 5 minutes
+    #window_size =  5# test
     y_default = 100     #when the y-values inside the window < y_default, the y-axis maximum = y_default (i.e. minimum y_axis range)
     #graph.axis([0, window_size+100, 0, y_default])  #setting the initial plot dimensions
 
@@ -63,16 +64,17 @@ if __name__ == '__main__':
         user.bytes_rcvd_per_sec.extend([0]*window_size)
         user.bytes_sent_per_sec.extend([0]*window_size)
         user.time_stamp.extend([time_now]*window_size)
-    start_time = time.monotonic()
+    #start_time = time.monotonic()
     try:
         while True:
-            shift_left(user.bytes_rcvd_per_sec)
-            shift_left(user.bytes_sent_per_sec)
-            shift_left(user.time_stamp)
+            for user in user_list:
+                shift_left(user.bytes_rcvd_per_sec)
+                shift_left(user.bytes_sent_per_sec)
+                shift_left(user.time_stamp)
 
-            relative_start_time = time.monotonic()
+            #relative_start_time = time.monotonic()
             cap.sniff(timeout=sniff_duration)
-            delta_time = time.monotonic() - relative_start_time
+            #delta_time = time.monotonic() - relative_start_time
             captured_packets = cap._packets
             for pkt in captured_packets: #iterate through all sniffed packet
                 for user in user_list:  #iterate through user user_list
@@ -82,32 +84,34 @@ if __name__ == '__main__':
                         user.bytes_sent_per_sec[-1] += int(pkt.length)
             time_now = datetime.fromtimestamp(mktime(time.localtime()))
             for user in user_list: #get time average
-                user.bytes_rcvd_per_sec[-1] /= delta_time
-                user.bytes_sent_per_sec[-1] /= delta_time
+                user.bytes_rcvd_per_sec[-1] /= sniff_duration#delta_time
+                user.bytes_sent_per_sec[-1] /= sniff_duration#delta_time
                 user.time_stamp[-1] = time_now
-
+                # print('user: ',user.ip)
+                # print('rcvd: ',user.bytes_rcvd_per_sec)
+                # print('sent: ',user.bytes_sent_per_sec)
             #UPDATE PLOT
-
             graph.clear()   #clear the plot every iteration to give way to the new curves
+            y_max = 0
             for user in user_list:
                 graph.plot(mdates.date2num(user.time_stamp), user.bytes_sent_per_sec, label=user.ip + ' (sent)')
                 graph.plot(mdates.date2num(user.time_stamp), user.bytes_rcvd_per_sec, label=user.ip + ' (recv)')
-                graph.legend(loc='center left', bbox_to_anchor=(0.9, 0.5),fontsize='small')
-
-                plt.title('Wifi Usage per IP')# please add plot title here
-                plt.xlabel('Time')
-                plt.ylabel('Bytes per second')
-                plt.grid()
-                plt.gcf().autofmt_xdate()
-
-                y_max = max(user.bytes_sent_per_sec + user.bytes_rcvd_per_sec + [y_default])  #y_max = max(sent or received bytes or y_default)
+                y_max = max(user.bytes_sent_per_sec + user.bytes_rcvd_per_sec + [y_default] + [y_max])  #y_max = max(sent or received bytes or y_default)
                 plt.ylim(0, y_max + int(0.1*y_max))
-                graph.xaxis.set_major_locator(mdates.MinuteLocator())
-                graph.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
-                graph.xaxis.set_minor_locator(mdates.SecondLocator(interval=sniff_duration))
 
-                fig.canvas.draw()
-                plt.pause(0.001) # needed to be able to see plot
+            plt.title('Wifi Usage per IP')# please add plot title here
+            plt.xlabel('Time')
+            plt.ylabel('Bytes per second')
+            plt.grid()
+            plt.gcf().autofmt_xdate()
+
+            graph.legend(loc='center left', bbox_to_anchor=(0.9, 0.5),fontsize='small')
+            graph.xaxis.set_major_locator(mdates.MinuteLocator())
+            graph.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+            graph.xaxis.set_minor_locator(mdates.SecondLocator(interval=sniff_duration))
+
+            fig.canvas.draw()
+            plt.pause(0.001) # needed to be able to see plot
 
             cap.clear()
 
